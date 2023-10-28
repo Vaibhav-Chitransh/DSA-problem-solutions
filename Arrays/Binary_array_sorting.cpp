30222133263e21c773083f60d314d43aea6c386d
Vaibhav Chitransh <vaibhavchitranshbdn@gmail.com>
2023-10-28 11:36:24 +0530

    Binary array sorting

---
+++ Arrays/Binary_array_sorting.cpp
@@ -0,0 +1,29 @@
+void binSort(int A[], int N)
+    {
+        
+        int i=0;
+        int j=N-1;
+        
+        while(i < j) {
+            if(A[i] == 1 && A[j] == 0) {
+                swap(A[i], A[j]);
+                i++;
+                j--;
+            }
+            
+            else if(A[i] == 0 && A[j] == 0) {
+                i++;
+            }
+            
+            else if(A[i] == 1 && A[j] == 1) {
+                j--;
+            }
+            
+            else if(A[i] == 0 && A[j] == 1) {
+                i++;
+                j--;
+            }
+        }
+    }
+
+
