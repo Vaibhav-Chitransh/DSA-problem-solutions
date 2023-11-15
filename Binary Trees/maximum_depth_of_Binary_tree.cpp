8b93d3b056857a6423a12cf435225f3b58a2f527
Vaibhav Chitransh <vaibhavchitranshbdn@gmail.com>
2023-11-15 10:20:39 +0530

    maximum depth of Binary tree

---
+++ Binary Trees/maximum_depth_of_Binary_tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if(root == NULL) {
+            return 0;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        // int count = 0;
+
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for(int i=0; i<size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if(node -> left != NULL) {
+                    q.push(node -> left);
+                }
+
+                if(node -> right != NULL) {
+                    q.push(node -> right);
+                }
+
+                level.push_back(node -> val);
+                // count++;
+            }
+
+            ans.push_back(level);
+        }
+        return ans.size();
+    }
+};
